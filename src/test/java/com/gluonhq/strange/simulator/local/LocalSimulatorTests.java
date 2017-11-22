6f9e08fae465c56fec3b62d996fddd95820dbbda
Johan Vos <johan.vos@gluonhq.com>
2017-11-22 19:26:38 +0100

    Test the contents of the resulting vector in tests. Resolves #10

--- src/test/java/com/gluonhq/strange/simulator/local/LocalSimulatorTests.java
+++ src/test/java/com/gluonhq/strange/simulator/local/LocalSimulatorTests.java
@@ -14,7 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LocalSimulatorTests {
 
     private final LocalSimulator sim = new LocalSimulator();
-
+    private final double DELTA = .0001;
+    private final double SQRT2 = Math.sqrt(2.d)/2.d;
 
     @Test
     @DisplayName("Simple: One qubit, no gates")
@@ -23,9 +24,10 @@ class LocalSimulatorTests {
         Model model = Model.getInstance();
         model.setNQubits(1);
         model.setGatesForCircuit(0, List.of(Gate.IDENTITY));
-
         double[] res = sim.calculateResults(model);
         assertEquals( 2, res.length );
+        assertEquals(1.d, res[0], DELTA);
+        assertEquals(0.d, res[1], DELTA);
 
     }
 
@@ -36,8 +38,9 @@ class LocalSimulatorTests {
         model.setNQubits(1);
         model.setGatesForCircuit(0, List.of(Gate.NOT));
         double[] res = sim.calculateResults(model);
-
         assertEquals( 2, res.length );
+        assertEquals(0.d, res[0], DELTA);
+        assertEquals(1.d, res[1], DELTA);
 
     }
 
@@ -48,9 +51,9 @@ class LocalSimulatorTests {
         model.setNQubits(1);
         model.setGatesForCircuit(0,List.of(Gate.HADAMARD));
         double[] res = sim.calculateResults(model);
-
         assertEquals( 2, res.length );
-
+        assertEquals(SQRT2, res[0], DELTA);
+        assertEquals(SQRT2, res[1], DELTA);
     }
 
     @Test
@@ -62,9 +65,9 @@ class LocalSimulatorTests {
         List<Gate> gates = List.of(Gate.NOT, Gate.NOT);
         model.setGatesForCircuit(0, gates);
         double[] res = sim.calculateResults(model);
-
         assertEquals( 2, res.length );
-
+        assertEquals(1.d, res[0], DELTA);
+        assertEquals(0.d, res[1], DELTA);
     }
 
     @Test
@@ -77,9 +80,9 @@ class LocalSimulatorTests {
         List<Gate> gates = List.of(Gate.HADAMARD,Gate.HADAMARD,Gate.NOT);
         model.setGatesForCircuit(0,gates);
         double[] res = sim.calculateResults(model);
-
         assertEquals( 2, res.length );
-
+        assertEquals(0.d, res[0], DELTA);
+        assertEquals(1.d, res[1], DELTA);
     }
 
     @Test
@@ -91,8 +94,11 @@ class LocalSimulatorTests {
         GateConfig gates = GateConfig.of(List.of(Gate.IDENTITY), List.of(Gate.IDENTITY));
         model.setGates(gates);
         double[] res = sim.calculateResults(model);
-
         assertEquals( 4, res.length );
+        assertEquals(1.d, res[0], DELTA);
+        assertEquals(0.d, res[1], DELTA);
+        assertEquals(0.d, res[2], DELTA);
+        assertEquals(0.d, res[3], DELTA);
 
     }
 
@@ -105,9 +111,12 @@ class LocalSimulatorTests {
         GateConfig gates = GateConfig.of(List.of(Gate.NOT), List.of(Gate.IDENTITY));
         model.setGates(gates);
         double[] res = sim.calculateResults(model);
-
         assertEquals( 4, res.length );
 
+        assertEquals(0.d, res[0], DELTA);
+        assertEquals(0.d, res[1], DELTA);
+        assertEquals(1.d, res[2], DELTA);
+        assertEquals(0.d, res[3], DELTA);
     }
 
 
