a281402d2302514eb0047be7acb4409c9984cbf0
jose.pereda <jose.pereda@gluonhq.com>
2018-05-11 00:57:40 +0200

    Added GateSymbol, Play button and styling

--- src/main/java/com/gluonhq/strangefx/render/RenderEngine.java
+++ src/main/java/com/gluonhq/strangefx/render/RenderEngine.java
@@ -34,24 +34,18 @@ package com.gluonhq.strangefx.render;
 import com.gluonhq.strange.Gate;
 import com.gluonhq.strange.Program;
 import com.gluonhq.strange.Step;
-import com.gluonhq.strange.gate.X;
+import com.gluonhq.strange.ui.GateSymbol;
 import com.gluonhq.strange.ui.Main;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 import javafx.animation.TranslateTransition;
-import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -68,6 +62,8 @@ public class RenderEngine extends VBox {
     private List<Qubit3D> qubits = new LinkedList<>();
     public RenderEngine() {
         super(50);
+        setPadding(new Insets(50));
+        getStyleClass().add("qubit");
     }
     /**
      * Create a JavaFX node containing a visualisation of a program
@@ -76,8 +72,6 @@ public class RenderEngine extends VBox {
      */
     public static RenderEngine createNode(Program p) {
         RenderEngine answer = new RenderEngine();
-        answer.setTranslateX(50);
-        answer.setTranslateY(50);
         int nq = p.getNumberQubits();
         List<Step> steps = p.getSteps();
         answer.nSteps = steps.size();
@@ -88,8 +82,7 @@ public class RenderEngine extends VBox {
             pane[i] = new HBox();
             pane[i].setAlignment(Pos.CENTER_LEFT);
             Line l = new Line(0, 0, 200, 0);
-            l.setStrokeWidth(3);
-            l.setStroke(Color.BLUE);
+            l.getStyleClass().add("wire");
             Qubit3D qubit3D = new Qubit3D();
 
             pane[i].getChildren().addAll(qubit3D, l);
@@ -109,34 +102,35 @@ public class RenderEngine extends VBox {
             int idx = 0;
             for (Gate gate : gates) {
                 int mqi = gate.getMainQubitIndex();
-                Group g = new Group();
-                Rectangle gateui = new Rectangle(40, 40, Color.YELLOW);
-                Label l = new Label("?");
-                if (gate instanceof X) {
-                    l.setText("X");
-                }
-                g.getChildren().addAll(gateui, l);
-                g.setTranslateX(-200 + stepSize * (idx + 1));
-                pane[mqi].getChildren().add(g);
+                GateSymbol symbol = GateSymbol.of(gate, false);
+                symbol.setTranslateX(-200 + stepSize * (idx + 1));
+                pane[mqi].getChildren().add(symbol);
                 idx++;
             }
         }
         return answer;
     }
     
+    private static int counter = 0;
+    
     public void animate() {
-        TranslateTransition tt = new TranslateTransition(Duration.seconds(5), qubits.get(0));
+        final Qubit3D node = qubits.get(counter++ % qubits.size());
+        TranslateTransition tt = new TranslateTransition(Duration.seconds(5), node);
+        node.resetStep();
+        tt.setFromX(0);
         tt.setByX(nSteps * stepSize);
-        tt.play();
+        tt.playFromStart();
     }
     
     public static void showCircuit(Program p) {
         System.out.println("will show circuit for "+p);
         RenderEngine re = createNode(p);
         System.out.println("re = "+re);
-         Scene scene = new Scene(re, 800, 600);
+         
+        Scene scene = new Scene(re, 800, 600);
         scene.getStylesheets().add(Main.class.getResource("/styles.css").toExternalForm());
-                Stage stage = new Stage();
+                
+        Stage stage = new Stage();
         stage.setTitle("StrangeFX rendering");
         stage.setScene(scene);
         System.out.println("show stage...");
