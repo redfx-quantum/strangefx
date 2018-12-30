74309bb4b42f3cc6d2ce7f62883ccd62ade9a921
Johan Vos <johan@lodgon.com>
2018-12-30 15:43:46 +0100

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.gluonhq.strangefx {
+    requires java.logging;
+    requires javafx.controls;
+    requires com.gluonhq.strange;
+    requires org.fxyz3d.core;
+    // requires fontawesomefx.materialicons;
+    // requires charm.cloudlink.client;
+
+    exports com.gluonhq.strangefx.render;
+}
