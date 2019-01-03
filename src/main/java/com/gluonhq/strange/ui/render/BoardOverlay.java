be89005f7eac46014cf348cb8e92abdf7db7f348
Johan Vos <johan@lodgon.com>
2019-01-03 15:04:24 +0100

    visualise probabilities

--- src/main/java/com/gluonhq/strange/ui/render/BoardOverlay.java
+++ src/main/java/com/gluonhq/strange/ui/render/BoardOverlay.java
@@ -24,6 +24,41 @@ public class BoardOverlay extends Region {
     private void createProbability(Step s, GateSymbol symbol) {
         Gate gate = symbol.getGate();
         System.err.println("Prob for step "+s.getIndex());
+        Program program = s.getProgram();
+        Result result = program.getResult();
+        Complex[] ip = result.getIntermediateProbability(s.getIndex());
+        System.err.println(" = "+ip[3].abssqr());
+        int nq = program.getNumberQubits();
+        int N = 1<<nq;
+        double deltaY = (66.*nq-10+38)/N;
+        System.err.println("n = "+nq+" and N = "+N+", dY = "+deltaY);
+        this.symbol.boundsInParentProperty().addListener(new InvalidationListener() {
+
+            @Override
+            public void invalidated(Observable observable) {
+                BoardOverlay me = BoardOverlay.this;
+                me.getChildren().clear();
+                Bounds bp = symbol.getBoundsInParent();
+                Point2D base = symbol.localToScene(0, 0);
+                Rectangle rect2 = new Rectangle(base.getX() , base.getY(),40, 66 * nq-10+38);
+                rect2.setFill(Color.WHITE);
+                rect2.setStroke(Color.BLUE);
+                rect2.setStrokeWidth(1);
+                BoardOverlay.this.getChildren().add(rect2);
+
+                for (int i = 0; i < N; i++) {
+                    double startY = base.getY()+ i * deltaY;
+                    Rectangle minibar = new Rectangle(1+base.getX(), base.getY()+ i*deltaY, 38 * ip[i].abssqr(), deltaY-1);
+                    minibar.setFill(Color.GREEN);
+                    Line l = new Line(1+base.getX(),startY,base.getX()+39,startY);
+                    l.setFill(Color.LIGHTGRAY);
+                    l.setStrokeWidth(1);
+                    BoardOverlay.this.getChildren().add(l);
+                    BoardOverlay.this.getChildren().add(minibar);
+                }
+
+            }
+        });
     }
     private void createOracle(GateSymbol symbol) {
         this.symbol.boundsInParentProperty().addListener(new InvalidationListener() {
