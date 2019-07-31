e41bb8569f80448b00561ceb159f8e174d88f1f5
Johan Vos <johan.vos@gluonhq.com>
2019-07-31 20:37:06 +0200

    Probgraph (#34)
    * change the way the Renderer renders a JavaFX UI
    This allows for different UI's in a single application
    
    * start probability graph
    
    * Improve probability
    
    * Use released version of Strange
    
    * Use Strange 0.0.8

--- src/main/java/com/gluonhq/strangefx/render/Renderer.java
+++ src/main/java/com/gluonhq/strangefx/render/Renderer.java
@@ -3,10 +3,13 @@ package com.gluonhq.strangefx.render;
 import com.gluonhq.strange.Complex;
 import com.gluonhq.strange.Gate;
 import com.gluonhq.strange.Program;
+import com.gluonhq.strange.QuantumExecutionEnvironment;
 import com.gluonhq.strange.Qubit;
+import com.gluonhq.strange.Result;
 import com.gluonhq.strange.Step;
 import com.gluonhq.strange.gate.Identity;
 import com.gluonhq.strange.gate.Oracle;
+import com.gluonhq.strange.local.SimpleQuantumExecutionEnvironment;
 import com.gluonhq.strange.simulator.Model;
 import com.gluonhq.strange.ui.GateSymbol;
 import com.gluonhq.strange.ui.Main;
@@ -14,101 +17,171 @@ import com.gluonhq.strange.ui.QubitBoard;
 import com.gluonhq.strange.ui.QubitFlow;
 import com.gluonhq.strange.ui.render.BoardOverlay;
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Renderer {
 
-    static {
-        Platform.startup(() -> {
-            System.err.println("JavaFX Platform initialized");
-        });
-   }
+    private static Stage myStage;
 
-    public static void renderProgram (Program p) {
+    static {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("JavaFX Platform initialized");
+            }
+        };
+        Platform.startup(r);
+    }
+
+    public static void renderProgram(Program p) {
         Platform.runLater(() -> showProgram(p));
     }
 
-    public static void showProbabilities (Program p, int count) {
-
+    public static void showProbabilities(Program p, int count) {
+        System.err.println("SHOWPROB, count = " + count);
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        int nq = p.getNumberQubits();
+        int[] counter = new int[1 << nq];
+        for (int i = 0; i < count; i++) {
+            Result result = simulator.runProgram(p);
+            int prob = result.getMeasuredProbability();
+            counter[prob]++;
+        }
+        for (int i = 0; i < counter.length; i++) {
+            System.err.println("cnt [" + i + "]: " + counter[i]);
+        }
+        Platform.runLater(() -> renderMeasuredProbabilities(counter));
     }
 
-    public static void showProgram(Program program)  {
+    public static void showProgram(Program program) {
         int nQubits = program.getNumberQubits();
         Stage stage = new Stage();
-            stage.setTitle("StrangeFX");
-            QubitBoard board = new QubitBoard(nQubits);
-            List<GateSymbol> multiWires = new LinkedList();
-            List<GateSymbol> probabilities = new LinkedList();
-            List<BoardOverlay> boardOverlays = new LinkedList<>();
-            ObservableList<QubitFlow> wires = board.getWires();
-            for (Step s : program.getSteps()) {
-                boolean[] gotit = new boolean[nQubits];
-                for (Gate gate : s.getGates()) {
-                    int qb = gate.getMainQubitIndex();
-                    gotit[qb] = true;
-                    QubitFlow wire = wires.get(qb);
-                    wire.setMinWidth(480);
-                    GateSymbol symbol = wire.addGate(gate);
-                    if (symbol.spanWires > 1) {
-                        if (gate instanceof Oracle) {
-                            multiWires.add(symbol);
-                            BoardOverlay overlay = new BoardOverlay(s, symbol);
-                            boardOverlays.add(overlay);
-                            board.addOverlay(overlay);
-                        } else {
-                            gate.getAffectedQubitIndex().stream().filter(e -> e!= qb).
-                                    forEach(a -> {
-                                        QubitFlow q = wires.get(a);
-                                        GateSymbol symbol2 = q.addAdditonalGateSymbol(gate, 1);
-                                        BoardOverlay overlay = new BoardOverlay(s, symbol, symbol2);
-                                        boardOverlays.add(overlay);
-                                        board.addOverlay(overlay);
-                                    });
-                        }
-
-                    }
-                    if (symbol.probability) {
-                        probabilities.add(symbol);
+        myStage = stage;
+        stage.setTitle("StrangeFX");
+        QubitBoard board = new QubitBoard(nQubits);
+        List<GateSymbol> multiWires = new LinkedList();
+        List<GateSymbol> probabilities = new LinkedList();
+        List<BoardOverlay> boardOverlays = new LinkedList<>();
+        ObservableList<QubitFlow> wires = board.getWires();
+        for (Step s : program.getSteps()) {
+            boolean[] gotit = new boolean[nQubits];
+            for (Gate gate : s.getGates()) {
+                int qb = gate.getMainQubitIndex();
+                gotit[qb] = true;
+                QubitFlow wire = wires.get(qb);
+                wire.setMinWidth(480);
+                GateSymbol symbol = wire.addGate(gate);
+                if (symbol.spanWires > 1) {
+                    if (gate instanceof Oracle) {
+                        multiWires.add(symbol);
                         BoardOverlay overlay = new BoardOverlay(s, symbol);
                         boardOverlays.add(overlay);
                         board.addOverlay(overlay);
+                    } else {
+                        gate.getAffectedQubitIndex().stream().filter(e -> e != qb).
+                                forEach(a -> {
+                                    QubitFlow q = wires.get(a);
+                                    GateSymbol symbol2 = q.addAdditonalGateSymbol(gate, 1);
+                                    BoardOverlay overlay = new BoardOverlay(s, symbol, symbol2);
+                                    boardOverlays.add(overlay);
+                                    board.addOverlay(overlay);
+                                });
                     }
+
                 }
-                for (int i = 0; i < nQubits; i++) {
-                    if (!gotit[i]) {
-                        QubitFlow wire = wires.get(i);
-                        wire.addGate(new Identity());
-                    }
+                if (symbol.probability) {
+                    probabilities.add(symbol);
+                    BoardOverlay overlay = new BoardOverlay(s, symbol);
+                    boardOverlays.add(overlay);
+                    board.addOverlay(overlay);
                 }
             }
-            ObservableList<Double> endStates = Model.getInstance().getEndStates();
-            Qubit[] qubits = program.getResult().getQubits();
-            Complex[] probability = program.getResult().getProbability();
-            Double[] endValues = new Double[probability.length];
-            int idx = 0;
-            for (Qubit qubit: qubits) {
-                endValues[idx++] = qubit.getProbability();
+            for (int i = 0; i < nQubits; i++) {
+                if (!gotit[i]) {
+                    QubitFlow wire = wires.get(i);
+                    wire.addGate(new Identity());
+                }
             }
-            endStates.setAll(endValues);
+        }
+        ObservableList<Double> endStates = Model.getInstance().getEndStates();
+        Qubit[] qubits = program.getResult().getQubits();
+        Complex[] probability = program.getResult().getProbability();
+        Double[] endValues = new Double[probability.length];
+        int idx = 0;
+        for (Qubit qubit : qubits) {
+            endValues[idx++] = qubit.getProbability();
+        }
+        endStates.setAll(endValues);
 
 //            for (GateSymbol symbol : multiWires) {
 //                BoardOverlay overlay = new BoardOverlay(symbol);
 //                board.addOverlay(overlay);
 //            }
-            Scene scene = new Scene(board);
-            scene.getStylesheets().add(Main.class.getResource("/styles.css").toExternalForm());
-            stage.setScene(scene);
+        VBox vbox = new VBox(40);
+        vbox.getChildren().add(board);
+        Scene scene = new Scene(vbox);
+        scene.getStylesheets().add(Main.class.getResource("/styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void renderMeasuredProbabilities(int[] results) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        BarChart<String, Integer> barChart = new BarChart(xAxis, yAxis);
+        barChart.setData(getChartData(results));
+        barChart.setTitle("Measured probability distribution");
+        StackPane root = new StackPane();
+        root.getChildren().add(barChart);
+        if (myStage != null) {
+            Scene oldscene = myStage.getScene();
+            VBox box = (VBox)(oldscene.getRoot());
+            oldscene.setRoot(new StackPane());
+            box.getChildren().add(root);
+            Scene newScene = new Scene(box);
+            newScene.getStylesheets().add(Main.class.getResource("/styles.css").toExternalForm());
+            myStage.setScene(newScene);
+        } else {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 640, 480));
             stage.show();
         }
 
-//        public void launchMe() {
-//            RenderApplication.launch();
-//        }
-   // }
+    }
+
+    private static ObservableList<XYChart.Series<String, Integer>> getChartData(int[] results) {
+        ObservableList<XYChart.Series<String, Integer>> answer = FXCollections.observableArrayList();
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        series.setName("occurences");
+        answer.add(series);
+        for (int i = 0; i < results.length; i++) {
+            series.getData().add(new XYChart.Data<>(getFixedBinaryString(i, (int) (Math.log(results.length) / Math.log(2))), results[i]));
+        }
+        return answer;
+    }
+
+    private static String getFixedBinaryString(int i, int w) {
+        StringBuffer buff = new StringBuffer(Integer.toBinaryString(i));
+        while (buff.length() < w) {
+            buff.insert(0, "0");
+        }
+        return buff.toString();
+    }
+
 
 }
